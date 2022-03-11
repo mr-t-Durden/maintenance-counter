2003716e8566e253459ab2dbf9b2658b9848b7f2
mr-t-Durden <67805487+mr-t-Durden@users.noreply.github.com>
2022-03-11 13:10:15 +0100

    Setup folder structure and App.css

---
+++ src/components/app/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import logo from '../../img/b40720e431d141939b3a39363b49659b.png';
+
+function App() {
+  return (
+    <div className='box'>
+      <header>
+        <img className="logo" src={logo} alt="Maintenance Counter logo" />
+      </header>
+      <main>
+
+        {/* <MaintenanceIntervals />
+        <Incrementor /> */}
+      </main>
+      <footer>
+        Impressum ...
+      </footer>
+    </div>
+  );
+}
+
+export default App;

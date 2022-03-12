e0ae676fb0b7d02ff317ea7bcbfe58ce71c827a8
mr-t-Durden <67805487+mr-t-Durden@users.noreply.github.com>
2022-03-12 09:48:49 +0100

    Implement MaintenanceIntervals.js

---
+++ src/components/maintenanceintervals/MaintenanceIntervals.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './MaintenanceIntervals.css';
+
+function MaintenanceIntervals(props) {
+    return (
+        <div className="MaintenanceIntervals">
+            {
+                props.intervals.map(
+                    (interval) => {
+                        return <MaintenanceBar 
+                            interval={interval}
+                            onRemove={props.removeInterval}
+                            onClick={props.setIntervalActive}
+                            onDone={props.resetIntervalProgress}
+                        />;
+                    }
+                )
+            }
+        </div>
+    );
+}
+
+export default MaintenanceIntervals;

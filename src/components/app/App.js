15d032695a40515c5c66fa3bec528ebbbfe6431e
mr-t-Durden <67805487+mr-t-Durden@users.noreply.github.com>
2022-03-12 09:12:42 +0100

    Implement changeActiveIntervalsProgress in App.js

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,30 @@
 import './App.css';
-import React from 'react';
+import React, {useState} from 'react';
 import logo from '../../img/b40720e431d141939b3a39363b49659b.png';
 import Incrementor from '../incrementor/Incrementor';
 
 function App() {
+  const [intervals, setIntervals] = useState([
+    {
+      id: 1,
+      name: 'Oil bicycle chain',
+      currentValue: 0,
+      totalValue: 300,
+      active: true,
+      complete: false
+    }
+  ]);
+
+  function changeActiveIntervalsProgress(changeValue) {
+    const activeIntervals = intervals.filter( 
+      interval => interval.active 
+    );
+
+    activeIntervals.forEach( 
+      interval => interval.currentValue += changeValue
+    );
+  }
+
   return (
     <div className='box'>
       <header>
@@ -11,7 +32,9 @@ function App() {
       </header>
       <main>
         {/* <MaintenanceIntervals /> */}
-        <Incrementor />
+        <Incrementor 
+          changeIntervalProgress={changeActiveIntervalsProgress} 
+        />
       </main>
       <footer>
         Impressum ...
